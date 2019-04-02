49d569cf31093cc7f463e15e8757d0e0a5d642b4
Sebastian Berge <sebastian@berge.im>
2019-04-02 18:53:47 +0200

    :construction: Work in progress

---
+++ src/main/java/hvl/StartServlet.java
@@ -0,0 +1,24 @@
+package hvl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/webapp")
+public class StartServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
